d3f09fdcbb30b8823169323cf4e1751c82edc67f
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2023-07-21 17:24:47 +0200

    ConcurrentUtils: add RunnableCallable task wrapper for completableFutureSupplyAsync(...)

--- src/main/java/pl/morgwai/base/util/concurrent/ConcurrentUtils.java
+++ src/main/java/pl/morgwai/base/util/concurrent/ConcurrentUtils.java
@@ -20,19 +20,44 @@ public interface ConcurrentUtils {
 	 * {@link CompletableFuture#handle(BiFunction) handle(...)} /
 	 * {@link CompletableFuture#whenComplete(BiConsumer) whenComplete(...)} /
 	 * {@link CompletableFuture#exceptionally(Function) exceptionally(...)} chained calls.
+	 * <p>
+	 * In case {@code executor} rejects {@code task}, its {@link RejectedExecutionHandler} will
+	 * receive {@code task} wrapped with a {@link RunnableCallable}.</p>
 	 */
 	static <T> CompletableFuture<T> completableFutureSupplyAsync(
-			Callable<T> task, Executor executor) {
+		Callable<T> task,
+		Executor executor
+	) {
 		final var result = new CompletableFuture<T>();
-		executor.execute(
-			() -> {
+		executor.execute(new RunnableCallable<>(task) {
+			@Override public void run() {
 				try {
-					result.complete(task.call());
+					result.complete(wrappedTask.call());
 				} catch (Exception e) {
 					result.completeExceptionally(e);
 				}
 			}
-		);
+		});
 		return result;
+	}
+
+
+
+	/**
+	 * Wrapper for {@link Callable} tasks passed to
+	 * {@link #completableFutureSupplyAsync(Callable, Executor)}.
+	 */
+	abstract class RunnableCallable<T> implements Runnable {
+
+		public Callable<T> getWrappedTask() { return wrappedTask; }
+		public final Callable<T> wrappedTask;
+
+		public RunnableCallable(Callable<T> taskToWrap) {
+			this.wrappedTask = taskToWrap;
+		}
+
+		@Override public String toString() {
+			return wrappedTask.toString();
+		}
 	}
 }
--- src/test/java/pl/morgwai/base/util/concurrent/ConcurrentUtilsTest.java
+++ src/test/java/pl/morgwai/base/util/concurrent/ConcurrentUtilsTest.java
@@ -4,6 +4,7 @@ package pl.morgwai.base.util.concurrent;
 import java.util.concurrent.*;
 
 import org.junit.Test;
+import pl.morgwai.base.util.concurrent.ConcurrentUtils.RunnableCallable;
 
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
@@ -15,16 +16,16 @@ public class ConcurrentUtilsTest {
 
 
 	@Test
-	public void testCompletableFutureSupplyAsync() throws InterruptedException {
+	public void testCompletableFutureSupplyAsyncThrowing() throws InterruptedException {
 		final var thrown = new Exception("thrown");
 		final Throwable[] caughtHolder = new Throwable[1];
 		final var completionLatch = new CountDownLatch(1);
 
-		final var completableFuture = ConcurrentUtils.completableFutureSupplyAsync(
+		final var execution = ConcurrentUtils.completableFutureSupplyAsync(
 			() -> { throw thrown; },
 			Executors.newSingleThreadExecutor()
 		);
-		completableFuture.whenComplete(
+		execution.whenComplete(
 			(result, caught) -> {
 				if (result == null) caughtHolder[0] = caught;
 				completionLatch.countDown();
@@ -33,8 +34,50 @@ public class ConcurrentUtilsTest {
 
 		assertTrue("the Callable task should complete",
 				completionLatch.await(50L, TimeUnit.MILLISECONDS));
-		assertTrue("completableFuture should be marked as done", completableFuture.isDone());
+		assertTrue("execution should be marked as done", execution.isDone());
 		assertSame("caught exception should be the same as thrown by the Callable task",
 				thrown, caughtHolder[0]);
+	}
+
+
+
+	@Test
+	public void testCompletableFutureSupplyAsync() throws Exception {
+		final var result = "result";
+		final var testTask = new Callable<String>() {
+			@Override public String call() {
+				return result;
+			}
+			@Override public String toString() {
+				return "testTask";
+			}
+		};
+		final var executor = new IntrospectiveExecutor();
+
+		final var execution = ConcurrentUtils.completableFutureSupplyAsync(testTask, executor);
+		assertSame("result of execution should be the same as returned by testTask",
+				result, execution.get(50L, TimeUnit.MILLISECONDS));
+		assertTrue("capturedTask should be a RunnableCallable instance",
+				executor.capturedTask instanceof RunnableCallable);
+		@SuppressWarnings("unchecked")
+		final var capturedTask = (RunnableCallable<String>) executor.capturedTask;
+		assertSame("capturedTask should be wrapping testTask",
+				testTask, capturedTask.getWrappedTask());
+		assertSame("RunnableCallable should delegate toString to the original task",
+				testTask.toString(), executor.capturedTask.toString());
+	}
+
+	static class IntrospectiveExecutor extends ThreadPoolExecutor {
+
+		Runnable capturedTask;
+
+		public IntrospectiveExecutor() {
+			super(1, 1, 0L, TimeUnit.DAYS, new LinkedBlockingQueue<>(1));
+		}
+
+		@Override
+		protected void beforeExecute(Thread worker, Runnable task) {
+			capturedTask = task;
+		}
 	}
 }
