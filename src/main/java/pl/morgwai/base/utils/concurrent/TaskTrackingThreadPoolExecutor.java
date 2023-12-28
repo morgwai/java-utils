fdbc313df46e1e91ff73526e519cfe5cab473caf
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2023-12-28 21:21:11 +0100

    TaskTrackingThreadPoolExecutor: add some convenient constructors

--- src/main/java/pl/morgwai/base/utils/concurrent/TaskTrackingThreadPoolExecutor.java
+++ src/main/java/pl/morgwai/base/utils/concurrent/TaskTrackingThreadPoolExecutor.java
@@ -3,6 +3,8 @@ package pl.morgwai.base.utils.concurrent;
 
 import java.util.concurrent.*;
 
+import static java.util.concurrent.TimeUnit.DAYS;
+
 
 
 /** A {@link TaskTrackingExecutor} based on a {@link ThreadPoolExecutor}. */
@@ -15,21 +17,12 @@ public class TaskTrackingThreadPoolExecutor extends ThreadPoolExecutor
 
 
 
-	public TaskTrackingThreadPoolExecutor(
-		int corePoolSize,
-		int maximumPoolSize,
-		long keepAliveTime,
-		TimeUnit unit,
-		BlockingQueue<Runnable> workQueue
-	) {
-		super(
-			corePoolSize,
-			maximumPoolSize,
-			keepAliveTime,
-			unit,
-			workQueue
-		);
-		taskTrackingDecorator = new TaskTrackingExecutorDecorator(this, false, corePoolSize);
+	/**
+	 * Calls {@link #TaskTrackingThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue)
+	 * this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>())}.
+	 */
+	public TaskTrackingThreadPoolExecutor(int poolSize) {
+		this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>());
 	}
 
 
@@ -57,6 +50,33 @@ public class TaskTrackingThreadPoolExecutor extends ThreadPoolExecutor
 
 
 
+	/**
+	 * Calls {@link ThreadPoolExecutor#ThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue)
+	 * super} and sets up {@link TaskTrackingExecutor.TaskTrackingExecutorDecorator}.
+	 */
+	public TaskTrackingThreadPoolExecutor(
+		int corePoolSize,
+		int maximumPoolSize,
+		long keepAliveTime,
+		TimeUnit unit,
+		BlockingQueue<Runnable> workQueue
+	) {
+		super(
+			corePoolSize,
+			maximumPoolSize,
+			keepAliveTime,
+			unit,
+			workQueue
+		);
+		taskTrackingDecorator = new TaskTrackingExecutorDecorator(this, false, corePoolSize);
+	}
+
+
+
+	/**
+	 * Calls {@link ThreadPoolExecutor#ThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue,
+	 * ThreadFactory) super} and sets up {@link TaskTrackingExecutor.TaskTrackingExecutorDecorator}.
+	 */
 	public TaskTrackingThreadPoolExecutor(
 		int corePoolSize,
 		int maximumPoolSize,
@@ -78,6 +98,11 @@ public class TaskTrackingThreadPoolExecutor extends ThreadPoolExecutor
 
 
 
+	/**
+	 * Calls {@link ThreadPoolExecutor#ThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue,
+	 * RejectedExecutionHandler) super} and sets up
+	 * {@link TaskTrackingExecutor.TaskTrackingExecutorDecorator}.
+	 */
 	public TaskTrackingThreadPoolExecutor(
 		int corePoolSize,
 		int maximumPoolSize,
@@ -99,6 +124,11 @@ public class TaskTrackingThreadPoolExecutor extends ThreadPoolExecutor
 
 
 
+	/**
+	 * Calls {@link ThreadPoolExecutor#ThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue,
+	 * ThreadFactory, RejectedExecutionHandler) super} and sets up
+	 * {@link TaskTrackingExecutor.TaskTrackingExecutorDecorator}.
+	 */
 	public TaskTrackingThreadPoolExecutor(
 		int corePoolSize,
 		int maximumPoolSize,
@@ -118,5 +148,63 @@ public class TaskTrackingThreadPoolExecutor extends ThreadPoolExecutor
 			handler
 		);
 		taskTrackingDecorator = new TaskTrackingExecutorDecorator(this, false, corePoolSize);
+	}
+
+
+
+	/**
+	 * Calls {@link #TaskTrackingThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue)
+	 * this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>(queueSize))}.
+	 */
+	public TaskTrackingThreadPoolExecutor(int poolSize, int queueSize) {
+		this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>(queueSize));
+	}
+
+
+
+	/**
+	 * Calls {@link #TaskTrackingThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue)
+	 * this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>(queueSize), threadFactory)}.
+	 */
+	public TaskTrackingThreadPoolExecutor(int poolSize, int queueSize, ThreadFactory threadFactory)
+	{
+		this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>(queueSize), threadFactory);
+	}
+
+
+
+	/**
+	 * Calls {@link #TaskTrackingThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue)
+	 * this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>(queueSize), handler)}.
+	 */
+	public TaskTrackingThreadPoolExecutor(
+		int poolSize,
+		int queueSize,
+		RejectedExecutionHandler handler
+	) {
+		this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>(queueSize), handler);
+	}
+
+
+
+	/**
+	 * Calls {@link #TaskTrackingThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue)
+	 * this(poolSize, poolSize, 0L, DAYS, new LinkedBlockingQueue<>(queueSize), threadFactory,
+	 * handler)}.
+	 */
+	public TaskTrackingThreadPoolExecutor(
+		int poolSize,
+		int queueSize,
+		ThreadFactory threadFactory,
+		RejectedExecutionHandler handler
+	) {
+		this(
+			poolSize,
+			poolSize,
+			0L, DAYS,
+			new LinkedBlockingQueue<>(queueSize),
+			threadFactory,
+			handler
+		);
 	}
 }
